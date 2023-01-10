c9a83135c5ff5b6e94f65477102838a97ba407b0
Lucas David <89425766+LucasDavid80@users.noreply.github.com>
2023-01-09 21:14:57 -0300

    Criando classe Main

--- src/main/java/com/mycompany/teste/Main.java
+++ src/main/java/com/mycompany/teste/Main.java
@@ -1,14 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.mycompany.teste;
 
-/**
- *
- * @author lucas
- */
+
 public class Main {
-    
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
 }
